ea91bad99566f1dfa5a49145f1abf8bb94988610
yantavares <yantdo1@gmail.com>
2023-01-08 12:28:25 -0300

    test2

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,3 +1,14 @@
 class Queens:
     def __init__(self, board):
         self.board = board
+
+    def solve(self):
+        countQueens = 0
+        countSize = 0
+        if self.board == []:
+            return -1
+        for i in self.board:
+            countQueens += i.count("1")
+            countSize += 1
+        if countQueens != 8 or countSize != 8:
+            return -1
--- testa_oito_rainhas.py
+++ testa_oito_rainhas.py
@@ -1,5 +1,13 @@
 from oito_rainhas import Queens
 
-def test1():
+
+def test1():  # Tabuleiro vazio
     t1 = Queens([])
     assert t1.solve() == -1
+
+
+def test2():  # Sem damas
+    t2 = Queens(["00000000", "00000000",
+                 "00000000", "00000000",
+                 "00000000", "00000000",
+                 "00000000", "00000000"])
